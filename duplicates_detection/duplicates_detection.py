953719abd9987f6df6308ebe0cb599b79164b836
Aleksandra Wieczorek <aleksandrawieczorek@Aleksandras-MacBook-Air.local>
2024-09-02 14:42:00 +0200

    Deleting classes in both test and train directories

--- duplicates_detection/duplicates_detection.py
+++ duplicates_detection/duplicates_detection.py
@@ -1,26 +1,41 @@
 import os
 from PIL import Image
 import imagehash
+import shutil
 
 from dotenv import load_dotenv
 load_dotenv()
 
 PATH_TO_PLANTNET_300K = os.environ.get('PATH_TO_PLANTNET_300K')
-image_dir = os.path.join(PATH_TO_PLANTNET_300K, 'images/train')
+image_dir_train = os.path.join(PATH_TO_PLANTNET_300K, 'images/train')
+image_dir_test = os.path.join(PATH_TO_PLANTNET_300K, 'images/test')
+
 
 hashes = {}
 duplicates = []
 
-def find_duplicates(image_dir):
+def find_duplicates(image_dir_train):
+    dir_number = 0
     # Loop through all directories in the train directory
-    for selected_class in os.listdir(image_dir):
-        class_dir = os.path.join(image_dir, selected_class)
+    for selected_class in os.listdir(image_dir_train):
+        class_dir = os.path.join(image_dir_train, selected_class)
+        class_dir_test = os.path.join(image_dir_test, selected_class)
+
         if not os.path.isdir(class_dir):
             continue  # Skip if it's not a directory
-
+        
         file_count = len([name for name in os.listdir(class_dir) if name.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))])
-        print(f"Number of files in {selected_class} directory: {file_count}")
+        print(f"Number of files in directory {selected_class} : {file_count}")
+        if file_count < 200: 
+            try:
+                shutil.rmtree(class_dir_test)
+                shutil.rmtree(class_dir)
+                print(f"Deleted directory: {selected_class}")
+            except Exception as e:
+                print(f"Error deleting directory {selected_class}: {e}")
+            continue 
 
+        dir_number += 1
         for filename in os.listdir(class_dir):
             if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
                 filepath = os.path.join(class_dir, filename)
@@ -32,36 +47,25 @@ def find_duplicates(image_dir):
                     continue
 
                 if img_hash in hashes:
-                    duplicates.append((filename, hashes[img_hash], selected_class, file_count))
-
+                    duplicates.append((filename, hashes[img_hash], selected_class))
                     #Image.open(os.path.join(class_dir, hashes[img_hash])).show()
                     #Image.open(filepath).show()
                 else:
                     hashes[img_hash] = filename
+    print(f"Number of directories in train {dir_number}")
 
-find_duplicates(image_dir)
+find_duplicates(image_dir_train)
 
 if duplicates:
     print(f"Total number of duplicates found: {len(duplicates)}")
     print("Found duplicates:")
     for dup in duplicates:
-        if dup[3] > 200:
-            print(f"Duplicate in class {dup[2]}: {dup[0]} - Original: {dup[1]} - Deleting duplicate")
-            # duplicate_path = os.path.join(image_dir, dup[2], dup[0])
-            # try:
-            #     os.remove(duplicate_path)
-            #     print(f"Deleted duplicate file: {dup[0]}")
-            # except Exception as e:
-            #     print(f"Error deleting {dup[0]}: {e}")
-        else:
-            print(f"Duplicate in class {dup[2]}: {dup[0]} - Original: {dup[1]} - Flipping duplicate vertically")
-            # duplicate_path = os.path.join(image_dir, dup[2], dup[0])
-            # try:
-            #     img = Image.open(duplicate_path)
-            #     img = img.transpose(Image.FLIP_TOP_BOTTOM)
-            #     img.save(duplicate_path)
-            #     print(f"Flipped and saved duplicate file: {dup[0]}")
-            # except Exception as e:
-            #     print(f"Error flipping {dup[0]}: {e}")
+        print(f"Duplicate in class {dup[2]}: {dup[0]} - Original: {dup[1]} - Deleting duplicate")
+        duplicate_path = os.path.join(image_dir_train, dup[2], dup[0])
+        try:
+            os.remove(duplicate_path)
+            print(f"Deleted duplicate file: {dup[0]}")
+        except Exception as e:
+            print(f"Error deleting {dup[0]}: {e}")
 else:
     print("No duplicates found.")
--- resnext.py
+++ resnext.py
@@ -17,6 +17,7 @@ def main():
     
     transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(), 
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
